086cea6462e82a30541b86c8499d1fb90a1d5987
walkor <walkor@workerman.net>
2022-12-06 14:20:23 +0800

    save

--- src/plugin/admin/public/admin/js/common.js
+++ src/plugin/admin/public/admin/js/common.js
@@ -32,6 +32,29 @@ function toggleSearchFormShow()
     $('.top-search-from .toggle-btn a:first').addClass('layui-hide');
 }
 
+/**
+ * 获取控制器详细权限，并决定展示哪些按钮或dom元素
+ */
+layui.$(function () {
+    let $ = layui.$;
+    $.ajax({
+        url: "/app/admin/admin-rule/permission",
+        dataType: "json",
+        data: {controller: CONTROLLER},
+        success: function (res) {
+            let style = '';
+            layui.each(res.data || [], function (k, action) {
+                if (action === '*') {
+                    style = '*[permission]{display: initial}';
+                    return;
+                }
+                style += '*[permission="'+action+'"]{display: initial}';
+            });
+            $("head").append("<style>"+style+"</style>");
+        }
+    });
+});
+
 layui.$(function () {
     toggleSearchFormShow();
 });
