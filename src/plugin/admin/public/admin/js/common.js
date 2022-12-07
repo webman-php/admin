1cd535f0cd31b2bbf7683602b578fbbed940dcac
walkor <walkor@workerman.net>
2022-12-07 12:43:45 +0800

    save

--- src/plugin/admin/public/admin/js/common.js
+++ src/plugin/admin/public/admin/js/common.js
@@ -32,31 +32,6 @@ function toggleSearchFormShow()
     $('.top-search-from .toggle-btn a:first').addClass('layui-hide');
 }
 
-/**
- * 获取控制器详细权限，并决定展示哪些按钮或dom元素
- */
-layui.$(function () {
-    let $ = layui.$;
-    $.ajax({
-        url: "/app/admin/admin-rule/permission-codes",
-        dataType: "json",
-        success: function (res) {
-            let style = '';
-            let codes = res.data || [];
-            // codes里有*，说明是超级管理员，拥有所有权限
-            if (codes.indexOf('*') !== -1) {
-                $("head").append("<style>*[permission]{display: initial}</style>");
-                return;
-            }
-            // 细分权限
-            layui.each(codes, function (k, code) {
-                codes[k] = '*[permission^="'+code+'"]';
-            });
-            $("head").append("<style>"+codes.join(",")+"{display: initial}</style>");
-        }
-    });
-});
-
 layui.$(function () {
     toggleSearchFormShow();
 });
