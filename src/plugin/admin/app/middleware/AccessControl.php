47047b9c653734c06232b9fb38b343d7f0788375
qnnp-me <wujipeng@foxmail.com>
2023-03-04 20:00:30 +0800

    避免接入admin的时候无权限的情况下调用不到admin的403文件而直接报错的情况

--- src/plugin/admin/app/middleware/AccessControl.php
+++ src/plugin/admin/app/middleware/AccessControl.php
@@ -21,11 +21,6 @@ class AccessControl implements MiddlewareInterface
         $controller = $request->controller;
         $action = $request->action;
 
-        $plugin=$request->plugin;
-        $app=$request->app;
-        $request->plugin='admin';
-        $request->app='';
-
         $code = 0;
         $msg = '';
         if (!Auth::canAccess($controller, $action, $code, $msg)) {
@@ -42,14 +37,13 @@ class AccessControl implements MiddlewareInterface
 EOF
                     );
                 } else {
+                    $request->app = '';
                     $request->plugin = 'admin';
                     $response = view('common/error/403')->withStatus(403);
                 }
             }
 
         } else {
-            $request->plugin=$plugin;
-            $request->app=$app;
             $response = $request->method() == 'OPTIONS' ? response('') : $handler($request);
         }
 
