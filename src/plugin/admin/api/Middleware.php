c8414d2e0b2e58e80a6d6ac8fe9181ad37426bea
walkor <walkor@users.noreply.github.com>
2023-03-10 21:40:36 +0800

    Update Middleware.php

--- src/plugin/admin/api/Middleware.php
+++ src/plugin/admin/api/Middleware.php
@@ -1,13 +1,14 @@
 <?php
 namespace plugin\admin\api;
 
+use ReflectionException;
 use Webman\Http\Request;
 use Webman\Http\Response;
 use Webman\MiddlewareInterface;
 use support\exception\BusinessException;
 
 /**
- * 对外提供的webman-admin鉴权中间件
+ * 对外提供的鉴权中间件
  */
 class Middleware implements MiddlewareInterface
 {
@@ -16,7 +17,7 @@ class Middleware implements MiddlewareInterface
      * @param Request $request
      * @param callable $handler
      * @return Response
-     * @throws \ReflectionException
+     * @throws ReflectionException
      * @throws BusinessException
      */
     public function process(Request $request, callable $handler): Response
@@ -30,7 +31,20 @@ class Middleware implements MiddlewareInterface
             if ($request->expectsJson()) {
                 $response = json(['code' => $code, 'msg' => $msg, 'type' => 'error']);
             } else {
-                $response = \response($msg, 401);
+                if ($code === 401) {
+                    $response = response(<<<EOF
+<script>
+    if (self !== top) {
+        parent.location.reload();
+    }
+</script>
+EOF
+                    );
+                } else {
+                    $request->app = '';
+                    $request->plugin = 'admin';
+                    $response = view('common/error/403')->withStatus(403);
+                }
             }
         } else {
             $response = $request->method() == 'OPTIONS' ? response('') : $handler($request);
