92b155128475102a1d21068f3ef64a3b2ae6f0d7
walkor <walkor@users.noreply.github.com>
2023-04-02 20:08:26 +0800

    Update InstallController.php

--- src/plugin/admin/app/controller/InstallController.php
+++ src/plugin/admin/app/controller/InstallController.php
@@ -381,6 +381,7 @@ EOF;
         }
         $params = [
             \PDO::MYSQL_ATTR_INIT_COMMAND => "set names utf8mb4",
+            \PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
             \PDO::ATTR_EMULATE_PREPARES => false,
             \PDO::ATTR_TIMEOUT => 5,
             \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
