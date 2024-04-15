d6533bde8129c2d6d259a8c1c362247b176f3a9c
walkor <walkor@workerman.net>
2024-04-15 09:25:36 +0800

    fix

--- src/plugin/admin/app/controller/UploadController.php
+++ src/plugin/admin/app/controller/UploadController.php
@@ -7,6 +7,7 @@ use Intervention\Image\ImageManagerStatic as Image;
 use plugin\admin\app\controller\Base;
 use plugin\admin\app\controller\Crud;
 use plugin\admin\app\model\Upload;
+use Random\RandomException;
 use support\exception\BusinessException;
 use support\Request;
 use support\Response;
@@ -283,17 +284,18 @@ class UploadController extends Crud
      * @param Request $request
      * @param $relative_dir
      * @return array
-     * @throws BusinessException
+     * @throws BusinessException|RandomException
      */
     protected function base(Request $request, $relative_dir): array
     {
-        $relative_dir = ltrim($relative_dir, '/');
+        $relative_dir = ltrim($relative_dir, '\\/');
         $file = current($request->file());
         if (!$file || !$file->isValid()) {
             throw new BusinessException('未找到上传文件', 400);
         }
 
-        $base_dir = config('plugin.admin.app.public_path') . DIRECTORY_SEPARATOR ?: base_path() . '/plugin/admin/public/';
+        $admin_public_path = rtrim(config('plugin.admin.app.public_path', ''), '\\/');
+        $base_dir = $admin_public_path ? $admin_public_path . DIRECTORY_SEPARATOR : base_path() . '/plugin/admin/public/';
         $full_dir = $base_dir . $relative_dir;
         if (!is_dir($full_dir)) {
             mkdir($full_dir, 0777, true);
