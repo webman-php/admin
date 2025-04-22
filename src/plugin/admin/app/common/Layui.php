35440b178dbfffd4355a47e64c90669b27ac691c
阿沁 <youziyouzishu@gmail.com>
2025-04-22 14:46:13 +0800

    fix(admin): 修复图片列表字段为空时的报错问题
    - 在 Layui.php 文件中，对图片列表模板进行了修改
    -增加了对字段存在性的判断，避免在字段不存在时进行分割操作
    - 优化了代码结构，提高了代码的健壮性和可读性

--- src/plugin/admin/app/common/Layui.php
+++ src/plugin/admin/app/common/Layui.php
@@ -1070,12 +1070,17 @@ EOF;
 EOF;
                     } else {
                         $templet = <<<EOF
+
 		                templet: function (d) {
-                            const images = d['$field'].split(',');
-                            let html = '';
+		                    let html = '';
+		                    if(d['$field']){
+		                    const images = d['$field'].split(',');
+                        
                             for (let img of images) {
                                 html += '<img src="' + encodeURI(img.trim()) + '" style="max-width:32px;max-height:32px;" alt="" />';
                             }
+		                    }
+                            
                             return html;
 						}
 EOF;
