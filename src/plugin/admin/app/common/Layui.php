277e651b1bad60367607b2fb00c87d77575a9185
阿沁 <youziyouzishu@gmail.com>
2025-04-22 14:02:35 +0800

    refactor(admin): 优化图片上传组件的 multiple 属性处理逻辑- 将 $props['multiple'] 的处理方式从三元运算符改为使用 null 合并运算符 - 这种写法更加简洁，并且在 $props['multiple'] 未设置时默认将其设为 0

--- src/plugin/admin/app/common/Layui.php
+++ src/plugin/admin/app/common/Layui.php
@@ -369,7 +369,7 @@ EOF;
         [$label, $field, $value, $props, $verify_string, $required_string, $class] = $this->options($options);
         $props['acceptMime'] = $props['acceptMime'] ?? 'image/gif,image/jpeg,image/jpg,image/png';
         $props['url'] = $props['url'] ?? '/app/admin/upload/image';
-        $props['multiple'] = $props['multiple'] ? 1 : 0;
+        $props['multiple'] = $props['multiple'] ?? 0;
 
         $id = $this->createId($field);
 
